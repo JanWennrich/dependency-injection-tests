02cacae0a813d74c1d0d4758fb870f6375b13de9
Jan Wennrich <jan.wennrich@pcsg.de>
2023-07-29 22:07:15 +0200

    feat: try to use laravel container

--- run.php
+++ run.php
@@ -1,7 +1,16 @@
 <?php
 
+use Jan\DependencyInjectionTests\FooProvider;
+use Jan\DependencyInjectionTests\HelloWorld;
+use Jan\DependencyInjectionTests\MyFooProvider;
+
 require_once __DIR__ . '/vendor/autoload.php';
 
 /**
  * This file will use dependency injection to get an instance of class HelloWorld
  */
+// Register directories into the autoloader
+
+$container = new Illuminate\Container\Container();
+
+$container->get(HelloWorld::class)->sayHello();
