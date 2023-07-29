c0e74062d605e203c2be67f59b5e8d200f23d474
Jan Wennrich <jan.wennrich@pcsg.de>
2023-07-29 22:44:42 +0200

    feat implement DI using PHP-DI

--- run.php
+++ run.php
@@ -1,7 +1,25 @@
 <?php
 
+use Jan\DependencyInjectionTests\FooProvider;
+use Jan\DependencyInjectionTests\HelloWorld;
+use Jan\DependencyInjectionTests\MyFooProvider;
+
 require_once __DIR__ . '/vendor/autoload.php';
 
 /**
  * This file will use dependency injection to get an instance of class HelloWorld
  */
+$containerBuilder = new DI\ContainerBuilder();
+
+$containerBuilder->useAutowiring(true);
+
+$containerBuilder->addDefinitions([
+    FooProvider::class => DI\autowire(
+        MyFooProvider::class
+    )->constructorParameter('foo', 'php-di')
+]);
+
+$container = $containerBuilder->build();
+
+$helloWorld = $container->get(HelloWorld::class);
+$helloWorld->sayHello();
