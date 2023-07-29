f4d52f04d07c885b8e71af9469f7bbb5f9c7296f
Jan Wennrich <jan.wennrich@pcsg.de>
2023-07-29 22:31:34 +0200

    feat: implement DI using Aura.Di

--- run.php
+++ run.php
@@ -5,3 +5,17 @@ require_once __DIR__ . '/vendor/autoload.php';
 /**
  * This file will use dependency injection to get an instance of class HelloWorld
  */
+
+use Aura\Di\ContainerBuilder;
+use Jan\DependencyInjectionTests\FooProvider;
+use Jan\DependencyInjectionTests\HelloWorld;
+use Jan\DependencyInjectionTests\MyFooProvider;
+
+$builder = new ContainerBuilder();
+
+$di = $builder->newInstance($builder::AUTO_RESOLVE);
+
+// Aura.Di cannot resolve interfaces with only one class implementing them
+$di->types[FooProvider::class] = $di->lazyNew(MyFooProvider::class, ['aura.di']);
+
+$di->newInstance(HelloWorld::class)->sayHello();
